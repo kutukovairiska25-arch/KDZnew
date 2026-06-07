c274d1d1e7e32b9d868038cd8fe1e1f1a2a3f6e7
Irishka Kutukova <kutukovairiska25@gmail.com>
2026-06-07 17:50:46 +0500

    добавлен frontend-react

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI     # основной фреймворк для создания REST API
 from fastapi.middleware.cors import CORSMiddleware  # для запросов с других адресов
-from fastapi.staticfiles import StaticFiles   # позволяет FastAPI раздавать статические файлы
-from fastapi.responses import FileResponse  # возвращает клиенту файл (например, index.html)
-from pathlib import Path
-
 from .database import engine, Base
 from .routers import couriers, orders, auth
 from .createDbUsers import create_users   # создаёт тестовых пользователей
@@ -19,11 +15,26 @@ app = FastAPI(title="Candy Delivery API", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",  # React dev server
+        "http://127.0.0.1:5173",  # React dev server (альтернатива)
+        "http://localhost:3000",  # если будешь использовать другой порт
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# Подключаем роутер авторизации, курьеров и заказов
+app.include_router(auth.router)
+app.include_router(couriers.router)
+app.include_router(orders.router)
+
+'''
+from fastapi.staticfiles import StaticFiles   # позволяет FastAPI раздавать статические файлы
+from fastapi.responses import FileResponse  # возвращает клиенту файл (например, index.html)
+from pathlib import Path
 
 # __file__ — это путь к текущему файлу (main.py).
 # .resolve() — превращает относительный путь в абсолютный.
@@ -38,14 +49,11 @@ staticDir = Path(__file__).resolve().parents[2] / "frontend" / "static"
 async def main():
     index_path = staticDir / "index.html"
     return FileResponse(index_path)
-
+    
 # Монтируем папку static по адресу /static.
 # Если браузер запросит http://127.0.0.1:8000/static/newlogin.css,
 # FastAPI найдёт файл frontend/static/newlogin.css и отдаст его.
 # name="static" — внутреннее имя для этого "маршрута".
 app.mount("/static", StaticFiles(directory=staticDir), name="static")
 
-# Подключаем роутер авторизации, курьеров и заказов
-app.include_router(auth.router)
-app.include_router(couriers.router)
-app.include_router(orders.router)
+'''
