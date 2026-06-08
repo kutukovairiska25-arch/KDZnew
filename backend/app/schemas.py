03dba06218fa19f956b541ba7803448c340845a2
Irishka Kutukova <kutukovairiska25@gmail.com>
2026-06-08 18:17:38 +0500

    .

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -4,10 +4,12 @@ from typing import List, Optional
 
 
 # Схемы для авторизации
+
+# схема для входа в систему
 class UserLogin(BaseModel):
     username: str
     password: str
-
+# ответ после успешного логина
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str
@@ -16,17 +18,21 @@ class TokenResponse(BaseModel):
 
 
 # Схемы для Курьеров
+
+# схема импорта курьера
 class CourierItem(BaseModel):
     courier_id: int = Field(..., gt=0)
     courier_type: str = Field(..., pattern="^(foot|bike|car)$")
     regions: List[int] = Field(..., min_length=1)
     working_hours: List[str] = Field(..., min_length=1)
 
+# схема редактирования курьера
 class CourierUpdateRequest(BaseModel):
     courier_type: Optional[str] = None
     regions: Optional[List[int]] = None
     working_hours: Optional[List[str]] = None
 
+#
 class CourierGetResponse(BaseModel):
     courier_id: int
     courier_type: str
@@ -37,15 +43,19 @@ class CourierGetResponse(BaseModel):
 
 
 # Схемы для Заказов
+
+# схема импорта заказа
 class OrderItem(BaseModel):
     order_id: int = Field(..., gt=0)
     weight: float = Field(..., gt=0.0, le=50.0)
     region: int = Field(..., gt=0)
     delivery_hours: List[str] = Field(..., min_length=1)
 
+# схема назначения заказов
 class OrdersAssignPostRequest(BaseModel):
     courier_id: int = Field(..., gt=0)
 
+# схема завершения заказа
 class OrdersCompletePostRequest(BaseModel):
     courier_id: int = Field(..., gt=0)
     order_id: int = Field(..., gt=0)
